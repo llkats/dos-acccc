7cf08082caa575c4dba526438a38fb128923fe51
Lydia Kats <llkats@gmail.com>
2019-08-22 16:40:32 -0600

    move js file to public/folder

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ const buildSecondaryPages = (dir, templateName) => {
 }
 
 const commonConfig = merge([{
-  entry: './src/index.js',
+  entry: './public/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname)
