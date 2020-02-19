fa62cfe7137e8e636e5d9bbf2ff151a08691d132
Lydia Kats <llkats@gmail.com>
2020-02-18 21:51:05 -0800

    fix the secondary language filter!!

--- public/index.js
+++ public/index.js
@@ -1,8 +1,5 @@
 import List from 'list.js'
 
-// const secondaryLangs = {{{ secondaryLanguagesJSON }}}
-const langs = []
-
 // list.js: https://listjs.com/docs/
 const languagesList = new List('languages-list',
   {
